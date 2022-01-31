c779ce2fa55eff6d629048f8acd83db1cb572867
Jóhannes Rónaldsson <g.stekelenburg@outlook.com>
2022-01-31 18:51:25 +0000

    Update block views

--- blocks/content_list/view.php
+++ blocks/content_list/view.php
@@ -1,15 +1,21 @@
-<p>
-	<?= t(sprintf('Found %s records.', '<strong>' . $contents['total'] . '</strong>')) ?>
-</p>
-<ul>
-	<?php foreach($contents['items'] as $content): ?>
-		<?php /* Example response
-			<li>
-				<?php
-					$title = $content['properties'][array_search('title', array_column($content['properties'], 'identifier'))]['value'] ?? null;
-					echo $title;
-				?>
-			</li>
-		*/ ?>
-	<?php endforeach; ?>
-</ul>
+<?php if ($contents): ?>
+	<p>
+		<?= t(sprintf('Found %s records.', '<strong>' . $contents['total'] . '</strong>')) ?>
+	</p>
+	<ul>
+		<?php foreach($contents['items'] as $content): ?>
+			<?php /* Example response
+				<li>
+					<?php
+						$title = $content['properties'][array_search('title', array_column($content['properties'], 'identifier'))]['value'] ?? null;
+						echo $title;
+					?>
+				</li>
+			*/ ?>
+		<?php endforeach; ?>
+	</ul>
+<?php else: ?>
+	<p>
+		<?= t('Nothing to show.') ?>
+	</p>
+<?php endif; ?>
--- blocks/content_view/view.php
+++ blocks/content_view/view.php
@@ -1,6 +1,12 @@
-<div>
-	<?php /* Example response
-		$title = $content['properties'][array_search('title', array_column($content['properties'], 'identifier'))]['value'] ?? null;
-		echo $title;
-	*/ ?>
-</div>
+<?php if ($content): ?>
+	<div>
+		<?php /* Example response
+			$title = $content['properties'][array_search('title', array_column($content['properties'], 'identifier'))]['value'] ?? null;
+			echo $title;
+		*/ ?>
+	</div>
+<?php else: ?>
+	<p>
+		<?= t('Nothing to show.') ?>
+	</p>
+<?php endif; ?>
